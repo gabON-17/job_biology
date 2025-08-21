81beb750f58539faaec41f14cf52ca182ae23af0
Gabriel Henrique <gabrielbarros1704@gmail.com>
2025-08-20 22:03:28 -0300

    Adição de novas páginas e javascript

---
+++ src/js/index.js
@@ -0,0 +1,32 @@
+const buttom_next = document.getElementById('bottom-next')
+const buttom_back = document.getElementById('bottom-back')
+const span_error = document.getElementById('span-error')
+
+let page = Number(window.location.pathname[21])
+console.log(span_error)
+
+buttom_back.addEventListener('click', (e) => {
+   console.log('e 1')
+   e.preventDefault()
+   if (page === 1) {
+      span_error.className = 'error'
+      span_error.innerHTML = 'Error. Última página alcançada'
+      return
+   }
+
+   page--
+   window.location.href = `/src/html/pages/page-${page}.html`
+})
+
+buttom_next.addEventListener('click', (e) => {
+   console.log('e 2')
+   e.preventDefault()
+   if (page === 7) {
+      span_error.className = 'error'
+      span_error.innerHTML = 'Error. Última página alcançada'
+      return
+   }
+   page++
+   window.location.href = `/src/html/pages/page-${page}.html`
+})
+
