656f1d5616f966abce1fe81e1f343a5e07b5cc71
Gabriel Henrique <gabrielbarros1704@gmail.com>
2025-08-28 02:08:26 -0300

    Projeto final v1.3(Correção de bugs para deploy)

--- src/js/button.js
+++ src/js/button.js
@@ -2,24 +2,23 @@ const buttom_next = document.getElementById('bottom-next')
 const buttom_back = document.getElementById('bottom-back')
 const span_error = document.getElementById('span-error')
 
-let page = Number(window.location.pathname[21])
-console.log(span_error)
+let page = window.location.pathname[21]
 
+console.log(page)
 buttom_back.addEventListener('click', (e) => {
    e.preventDefault()
-   if (page === 1) {
+   if (page === "1") {
       span_error.className = 'error'
       span_error.innerHTML = 'Error. Última página alcançada'
       return
    }
-
    page--
    window.location.href = `/src/html/pages/page-${page}.html`
 })
 
 buttom_next.addEventListener('click', (e) => {
    e.preventDefault()
-   if (page === 7) {
+   if (page === "7") {
       span_error.className = 'error'
       span_error.innerHTML = 'Error. Última página alcançada'
       return
